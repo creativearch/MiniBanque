3861d50b2c5177df9daa42670ad4e44416c81c7b
creativearch <po.hercule@laposte.net>
2014-04-22 19:50:38 +0200

    Create OperationBancaireException.java

---
+++ OperationBancaireException.java
@@ -0,0 +1,19 @@
+package Minibanque;
+
+public class OperationBancaireException extends Exception {
+
+    public OperationBancaireException() {
+   	 System.out.println("Votre montant est nul ou négatif ! Impossible d'effectuer l'opération");
+    }
+    
+    public OperationBancaireException(){
+   	 System.out.println("Le solde est insufisant. Impossible d'effectuer l'opération");
+    }
+    
+    public OperationBancaireException(){
+   	 System.out.println("Le solde plafond est atteint. Impossible d'effectuer l'opération ");
+    }
+
+}
+
+
