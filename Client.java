faf2a7d9e429e91475fa6c3f0b44b7bfc16a94c7
creativearch <po.hercule@laposte.net>
2014-04-22 19:47:51 +0200

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,62 @@
+package Minibanque;
+
+import java.util.Arrays;
+
+/**
+ *Represente un client
+ *@author Rama
+ *@version 1.0
+ */    
+
+
+public class Client {
+    
+    private int idClient;
+    private String nomClient;
+    private CompteBancaire []comptes;
+    
+    /**
+     * Construit le client
+     * @since version 1.0
+     */
+    
+    public Client (int _idClient, String _nomClient){
+   	 this.idClient=_idClient;
+   	 this.nomClient=_nomClient;
+    }
+    
+    
+    /**
+     * Retourne l'identifiant du client courant
+     * @return Identifiant de l'autre
+     * @since version 1.0
+     */
+    
+    public int getClient(){
+   	 return this.idClient;
+    }
+    
+    /**
+     * Ajoute un compte au client
+     * @since version 1.0
+     */
+    
+    public void ajouterCompte(CompteBancaire num){
+   	 int i=0;
+   	 boolean test=false;
+   	 while (!test){
+   		 if (this.comptes[i]==null){
+   			 this.comptes[i]=num;
+   			 test=true;
+   		 }
+   	 i++;
+   	 }
+    }
+
+    public String toString() {
+   	 return "Client [idClient=" + idClient + ", nomClient=" + nomClient
+   			 + ", comptes=" + Arrays.toString(comptes) + "]";
+    }
+}
+
+
