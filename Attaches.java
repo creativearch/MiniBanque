660c12858f50c7343b6afebc7567abcfe66f492b
creativearch <po.hercule@laposte.net>
2014-05-11 16:21:15 +0200

    Update Attaches.java

--- Attaches.java
+++ Attaches.java
@@ -1,18 +1,40 @@
-package Minibanque; 
-/** 
+package Minibanque;
+
+import java.util.ArrayList;
+
+/**
  *Represente un attaché 
- *@author Rama 
- *@version 1.0 
- */  
- 
-public class Attaches { 
-  
-    protected int idAttaches; 
-    protected String nomAttaches; 
-    protected int []clients; 
-  
-    public Attaches (int _idAttaches, String _nomAttaches){ 
-     this.idAttaches=_idAttaches; 
-     this.nomAttaches=_nomAttaches; 
-    } 
-} 
+ *@author Rama
+ *@version 1.0
+ */	
+
+public class Attaches {
+	
+	protected int idAttaches; 
+	protected String nomAttaches; 
+	private ArrayList<Client> clients = new ArrayList<Client>();
+	
+	public Attaches (int _idAttaches, String _nomAttaches){
+		this.idAttaches=_idAttaches; 
+		this.nomAttaches=_nomAttaches;
+	}
+	
+	public void ajouterClient(Client _clients){
+		clients.add(_clients);
+	}
+	
+	public String toString(){
+		String listeClient = "";
+		for (Client c : this.clients)
+		{
+			listeClient += c+"\n";
+		}
+		return "Nom attache: "+this.nomAttaches+"\nListe des clients:\n"+listeClient;
+	}
+	
+	public Client getClient(int numClient){
+		return this.clients.get(numClient);
+	}
+	
+	
+}
