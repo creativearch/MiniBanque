ebe068422cc54a614d2049bff5e30212c2478537
creativearch <po.hercule@laposte.net>
2014-05-11 16:59:49 +0200

    Update Banque.java

--- Banque.java
+++ Banque.java
@@ -1,19 +1,47 @@
 package Minibanque;
 
-public class Banque {
 
-    public static void main(String[] args) {
-   	 Attaches a= new Attaches (01,"Dupond");
-   	 Attaches b= new Attaches (02,"Dupont");
-   	 Client c = new Client (01, "C");
-   	 Client d = new Client (02, "Douteau");
-   	 CompteCourant e = new CompteCourant(01, "compte courant");
-   	 CompteEpargne f = new CompteEpargne(01, "compte epargne", 50, 700);
-   	 c.ajouterCompte(e);
-   	 c.ajouterCompte(f);
-   	 c.toString();
-    }
+/**
+ * Banque est la classe représentant la banque contenant le main
+ *
+ * @author Rama DESPLATS
+ */
 
-}
+public class Banque
+{
 
+	/**
+	 * Main permettant de tester les différents exemples
+	 *
+	 *
+	 */
+	public static void main(String[] args)
+	{
+		Attaches ac1 = new Attaches(0,"acl1");
+		Attaches ac2 = new Attaches(1,"acl2");
+
+		Client c1 = new Client(0, "Client 1", ac1);
+		Client c2 = new Client(1, "C", ac2);
+		CompteCourant cc1 = new CompteCourant(0,"Compte courant de C");
+		CompteEpargne ce2 = new CompteEpargne(1,"Compte epargne de C",2,700);
+		c2.ajouterCompte(cc1);
+		c2.ajouterCompte(ce2);
+		
+		ac2.ajouterClient(c1);
+		ac2.ajouterClient(c2);
+
+		System.out.println(ac1);
+		System.out.println(ac2);
+		try
+		{
+			ac2.getClient(1).getCompte(0).crediterCompte(2000);
+			ac2.getClient(1).getCompte(1).crediterCompte(33);
+			ac2.getClient(1).getCompte(0).debiterCompte(500);
+		}
+		catch(OperationBancaireException e)	{}
+		try
+		{
+			ac2.getClient(1).getCompte(0).debiterCompte(2500);
+		}
+		catch(OperationBancaireException e)	{}
 
