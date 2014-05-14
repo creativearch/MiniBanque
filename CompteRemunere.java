9129adf3219e5c013b35688fc5caedc0b384c185
creativearch <po.hercule@laposte.net>
2014-05-14 17:04:49 +0200

    Update CompteRemunere.java

--- CompteRemunere.java
+++ CompteRemunere.java
@@ -4,40 +4,46 @@ package Minibanque;
  *Represente un compte remunéré qui hérite de compte bancaire
  *@author Rama
  *@version 1.0
- */    
+ */	
 
 public class CompteRemunere extends CompteBancaire {
 
-    protected float tauxderemuneration;
-    protected int soldeplafond;
-    
-    public CompteRemunere(int _idCompte, String _libelle, float _tauxderumeneration, int _soldeplafond) {
-   	 super(_idCompte, _libelle);
-   	 this.tauxderemuneration=_tauxderumeneration;
-   	 this.soldeplafond=_soldeplafond;
-    }
-    
-    public float getSolde() {
-   	 return this.solde;
-    }
-    
-    public void crediterCompte(float _solde){
-   	 if (this.solde+_solde>this.soldeplafond){
-   		 System.out.println("Vous avez saisi un solde supérieur au solde plafond");
-   	 }
-   	 else {
-   		 this.solde+=_solde;
-   	 }
-    }
-    
-    public String toString() {
-   	 return "CompteRemunere [tauxderemuneration=" + tauxderemuneration
-   			 + ", soldeplafond=" + soldeplafond + ", idCompte=" + idCompte
-   			 + ", libelle=" + libelle + ", solde=" + solde + "]";
-    }
-    
-    
+	protected float tauxderemuneration;
+	protected double soldeplafond; 
+	
+	public CompteRemunere(int _idCompte, String _libelle, float _tauxderumeneration, double _soldeplafond) {
+		super(_idCompte, _libelle);
+		this.tauxderemuneration=_tauxderumeneration;
+		this.soldeplafond=_soldeplafond;
+	}
+	
+	public double getSolde() { 
+		return this.solde; 
+	} 
+	
+	
+	public boolean crediterCompte(double _montant) throws OperationBancaireException{
+		this.operations.add(new Operation("Credit",this.operations.size(),"",_montant,this));
+		if(_montant<=0){
+			this.operations.get(this.operations.size()-1).setStatut("KO");
+			throw new OperationBancaireException("Le montant demandé est négatif.");
+		}
+		if(this.solde+_montant>this.soldeplafond)
+		{
+			this.operations.get(this.operations.size()-1).setStatut("KO");
+			throw new PlafondException("Le plafond est dépassé en ajoutant ce montant.");
+		}
+		this.solde += _montant;
+		this.operations.get(this.operations.size()-1).setStatut("OK");
+		return true;
+	}
+	
+	public String toString() {
+		return "CompteRemunere [tauxderemuneration=" + tauxderemuneration
+				+ ", soldeplafond=" + soldeplafond + ", idCompte=" + idCompte
+				+ ", libelle=" + libelle + ", solde=" + solde + "]";
+	}
+	
+	
 
 }
-
-
